af8e7c84c5eed7c69300a3f3508dcf44aad940b3
ganrajchavan <ganeshchavan117@gmail.com>
2022-08-05 15:40:31 +0530

    Emtity class 10: sms 10:Entity class created

---
+++ FirstGitProject/src/main/java/com/GaneshIt/FirstGitProjectApplication.java
@@ -0,0 +1,13 @@
+package com.GaneshIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstGitProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstGitProjectApplication.class, args);
+	}
+
+}
